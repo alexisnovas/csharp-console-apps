1ccc7b26452857642a66b04a14910a89ff30568d
Alexis Novas <alexismoises@hotmail.com>
2021-08-26 19:24:09 -0400

    Add Fibonnaci and FizzBuzz Apps

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, limite, i, auxiliar; 
+            Console.WriteLine("Ingrese el número de numeros de Fibonacci que desea mostrar");
+            limite = int.Parse(Console.ReadLine()); 
+            a = 0;
+            b = 1; 
+            for (i = 0; i < limite; i++)  
+            {
+                auxiliar = a;
+                a = b; 
+                b = auxiliar + a; 
+                Console.WriteLine(a); 
+            }
+            Console.ReadKey(); 
+        }
+    }
+}
---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+    Recorrer una cantidad de numeros N,
+    si el numero es multiplo de 3 imprimir 'fizz'
+    en cambio, si el numero es multiplo de 5
+    imprimir 'buzz'. Para los numeros que sean
+    multiplos de 3 y 5 imprimir 'FizzBuzz'.
+*/
+using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int limit = 0;
+
+            Console.Write("Please, enter the limit: ");
+            limit = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= limit; i++)
+            {
+                if(i % 3 == 0 && i % 5 == 0){
+                    Console.WriteLine($"{i}fizzbuzz");
+                }else if(i % 3 == 0){
+                    Console.WriteLine($"{i}fizz");
+                }else if(i % 5 == 0){
+                    Console.WriteLine($"{i}buzz");
+                }else{
+                    Console.WriteLine(i);
+                }
+
+                
+            }
+        }
+    }
+}
