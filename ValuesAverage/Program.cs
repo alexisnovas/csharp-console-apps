fba67cae293f495bf1af6c48a5817ba17ad8a653
Alexis Novas <alexismoises@hotmail.com>
2021-08-27 13:15:40 -0400

    Update Value Average file

--- ValuesAverage/Program.cs
+++ ValuesAverage/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace ValuesAverage
 {
@@ -7,18 +8,22 @@ namespace ValuesAverage
         static void Main(string[] args)
         {
            int numberLimit = 4;
-           double value = 0;
-           double sum = 0;
+           //double value = 0;
+           // double sum = 0;
+           double[] arrValues = new double [4];
 
            for (int i = 0; i < numberLimit; i++)
            {
                Console.Write($"Enter value #{i+1}: ");
-               value = double.Parse(Console.ReadLine());
-               sum = sum + value;
+               //value = double.Parse(Console.ReadLine());
+               //sum = sum + value;
+               arrValues[i] = double.Parse(Console.ReadLine());
            }
 
            //double average = sum/numberLimit; This could stay for make the code more understandable.
-           Console.Write($"The average is {sum/numberLimit}");
+           //Console.Write($"The average is {sum/numberLimit}");
+
+           Console.Write($"The average is {arrValues.Average()}");
 
 
         }
